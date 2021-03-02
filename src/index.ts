0d1232d46e36b97c6fde55d51a9fe997cd97baea
Dillon Redding <dillon.redding@jbhunt.com>
2021-03-02 17:13:37 -0600

    adding copyright header

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright (C) 2020  Dillon Redding
+ *
+ * See the full notice in the LICENSE file at the top level of the repository.
+ */
 export * from './action';
 export * from './field';
 export * from './link';
