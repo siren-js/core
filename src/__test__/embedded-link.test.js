7ef16af2cf655edb1577dd4437d03b180c7a4bff
Dillon Redding <dillon.redding@jbhunt.com>
2021-03-12 17:31:12 -0600

    requiring non-empty rel for embedded links

--- src/__test__/embedded-link.test.js
+++ src/__test__/embedded-link.test.js
@@ -1,11 +1,28 @@
 import { EmbeddedLink } from '../embedded-link';
 import { Link } from '../link';
 
+const rel = ['self'];
+const href = 'http://example.com';
+
 describe('EmbeddedLink', () => {
     it('should be instanceof Link', () => {
-        const link = new EmbeddedLink(['self'], 'http://example.com');
+        const link = new EmbeddedLink(rel, href);
 
         expect(link).toBeInstanceOf(EmbeddedLink);
         expect(link).toBeInstanceOf(Link);
+    });
+
+    it('should require non-empty rel', () => {
+        expect(() => new EmbeddedLink([], href)).toThrow(TypeError);
+        expect(() => new EmbeddedLink([42], href)).toThrow(TypeError);
+    });
+
+    it('should ignore empty rel', () => {
+        const link = new EmbeddedLink(rel, href);
+
+        [[], [42]].forEach((value) => {
+            link.rel = value;
+            expect(link.rel).toEqual(rel);
+        });
     });
 });
--- src/embedded-link.js
+++ src/embedded-link.js
@@ -1,7 +1,23 @@
 import { Link } from './link';
+import * as coerce from './util/coerce';
 
 export class EmbeddedLink extends Link {
     constructor(rel, href, options = {}) {
         super(rel, href, options);
+
+        if (this.rel.length === 0) {
+            throw new TypeError();
+        }
+    }
+
+    get rel() {
+        return super.rel;
+    }
+
+    set rel(value) {
+        const rel = coerce.toStringArray(value, this.rel);
+        if (rel.length > 0) {
+            super.rel = value;
+        }
     }
 }
